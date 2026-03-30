c44ec35989b6a2708a830e56712caa2eb15fe284
Sarang <sarang1221@gmail.com>
2026-03-30 10:30:47 +0530

    Feature: - flag changes position with the code, - bug --> onClick of enter at the end of code on the flagged line, the flag doesnt shift to next line

--- store/flagStore.js
+++ store/flagStore.js
@@ -1,21 +1,40 @@
 let flags = [];
 
 function getFlags() {
-	return flags;
+  return flags;
 }
 
 function addFlag(flag) {
-	if(!flag) return;
-    flags.push(flag);
+  if (!flag) return;
+  flags.push(flag);
 }
 
 function removeFlag(index = 0) {
-    if(index < 0 || index > flags.length) return;
-	flags.splice(index, 1);
+  if (index < 0 || index > flags.length) return;
+  flags.splice(index, 1);
+}
+
+function updateFlagPositions(uri, changedLine, lineDiff, isLineSplit) {
+  const flags = getFlags();
+
+  flags.forEach((flag) => {
+    if (flag.uri !== uri) return;
+
+    if (isLineSplit) {
+      if (flag.line > changedLine) {
+        flag.line += lineDiff;
+      }
+    } else {
+      if (flag.line >= changedLine) {
+        flag.line += lineDiff;
+      }
+    }
+  });
 }
 
 module.exports = {
-	getFlags,
-	addFlag,
-	removeFlag
+  getFlags,
+  addFlag,
+  removeFlag,
+  updateFlagPositions,
 };
